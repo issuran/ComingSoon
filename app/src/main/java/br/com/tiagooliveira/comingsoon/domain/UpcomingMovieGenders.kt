ac27bd03236a62b05a2d9482eae4c0e35f37506d
Tiago Oliveira <oliveiratif@gmail.com>
2018-04-30 15:22:25 -0300

    Service - Create API Service to make request using Retrofit

--- app/src/main/java/br/com/tiagooliveira/comingsoon/domain/UpcomingMovieGenders.kt
+++
@@ -1,6 +0,0 @@
-package br.com.tiagooliveira.comingsoon.domain
-
-import com.google.gson.annotations.SerializedName
-
-data class UpcomingMovieGenders(@SerializedName("id") val id : Int = 0,
-                                 @SerializedName("name") val gender : String)
--- app/src/main/java/br/com/tiagooliveira/comingsoon/utils/constants/Constants.kt
+++ app/src/main/java/br/com/tiagooliveira/comingsoon/utils/constants/Constants.kt
@@ -4,8 +4,10 @@ import org.jetbrains.annotations.NotNull
 
 class Constants{
     companion object {
-        val baseUrl = "https://api.themoviedb.org/3/movie"
+        val baseUrl = "https://api.themoviedb.org/"
         val baseImageUrl = "https://image.tmdb.org/t/p/w500"
         val apiKey = "76064bf9310e5eb114f1665c3cde25e9"
+        val sortByReleaseDate = "release_date.asc"
+        val sortByPopularity = "popularity.desc"
     }
 }
