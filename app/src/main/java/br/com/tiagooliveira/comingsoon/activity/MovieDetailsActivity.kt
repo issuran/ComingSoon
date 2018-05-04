ad3a076e1089f39a52dfa2603bf6f91d15fcbd27
Tiago Oliveira <oliveiratif@gmail.com>
2018-05-03 23:20:03 -0300

    Cleaning - Implemented extension and removed unnecessary code to make it easier to read

--- app/src/main/java/br/com/tiagooliveira/comingsoon/activity/MovieDetailsActivity.kt
+++ app/src/main/java/br/com/tiagooliveira/comingsoon/activity/MovieDetailsActivity.kt
@@ -4,25 +4,22 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.ImageView
 import android.widget.ListView
-import android.widget.TextView
 import br.com.tiagooliveira.comingsoon.R
 import br.com.tiagooliveira.comingsoon.adapter.UpcomingMovieDetailsAdapter
 import br.com.tiagooliveira.comingsoon.domain.UpcomingMovieDetail
 import br.com.tiagooliveira.comingsoon.service.RetrofitInitializer
 import br.com.tiagooliveira.comingsoon.utils.constants.Constants
-import com.squareup.picasso.Picasso
+import br.com.tiagooliveira.comingsoon.utils.extensions.loadImage
+import kotlinx.android.synthetic.main.content_movie_details.*
 import kotlinx.android.synthetic.main.movie_details_activity.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.lang.Exception
 
 class MovieDetailsActivity : AppCompatActivity() {
 
     lateinit var mContext : Context
-
 
     lateinit var listView: ListView
 
@@ -35,7 +32,7 @@ class MovieDetailsActivity : AppCompatActivity() {
 
         var movieId = 0
 
-        var bundle = intent.extras
+        val bundle = intent.extras
         if(bundle != null)
             movieId = bundle.getInt(Constants.movie_identifier)
 
@@ -55,36 +52,17 @@ class MovieDetailsActivity : AppCompatActivity() {
                 response?.let {
                     val upcomingMovie = it.body() as UpcomingMovieDetail
 
-                    listView = findViewById<ListView>(R.id.listViewGenrer)
-
                     val adapter = UpcomingMovieDetailsAdapter(mContext, upcomingMovie.genres)
-                    listView.adapter = adapter
-
-                    var title = findViewById<TextView>(R.id.tTitleDetail)
-                    var release = findViewById<TextView>(R.id.tReleaseDateDetail)
-                    var img = findViewById<ImageView>(R.id.imgMovieDetails)
-                    var description = findViewById<TextView>(R.id.tMovieDescription)
-                    var duration = findViewById<TextView>(R.id.tMovieDuration)
-
+                    listViewGenre.adapter = adapter
 
                     toolbar_layout.title = upcomingMovie.originalTitle
-                    title.text = upcomingMovie.originalTitle
-                    release.text = upcomingMovie.releaseDate
-                    description.text = upcomingMovie.overview
-                    duration.setText(mContext.getString(R.string.duration_time, upcomingMovie.runtime.toString()))
+                    tTitleDetail.text = upcomingMovie.originalTitle
+                    tReleaseDateDetail.text = upcomingMovie.releaseDate
+                    tMovieDescription.text = upcomingMovie.overview
+                    tMovieDuration.setText(mContext.getString(R.string.duration_time, upcomingMovie.runtime.toString()))
 
                     // Download the image and display ProgressBar
-                    Picasso.get()
-                            .load(Constants.baseImageUrl + upcomingMovie.posterPath)
-                            .placeholder(R.drawable.placeholder_movie)
-                            .error(R.drawable.placeholder_movie)
-                            .into(img, object : com.squareup.picasso.Callback {
-                                override fun onSuccess() {
-                                }
-
-                                override fun onError(e: Exception?) {
-                                }
-                            })
+                    imgMovieDetails.loadImage("${Constants.baseImageUrl}${upcomingMovie.posterPath}")
                 }
             }
 
